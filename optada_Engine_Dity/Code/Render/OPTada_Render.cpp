0dc21f33c2c876b7ad5b44064feb93a925e44794
optada <druzhinin_v_d@ukr.net>
2020-06-21 20:05:54 +0300

    Add depthStencil buffer

--- optada_Engine_Dity/Code/Render/OPTada_Render.cpp
+++ optada_Engine_Dity/Code/Render/OPTada_Render.cpp
@@ -7,6 +7,8 @@
 bool OPTada_Render::Initialization(HWND hwnd_, int countOfBackBuffers_, int workspaceWidth_, int workspaceHeight_, bool vSinc_, bool isWindowedMode_)
 {
 
+    // ---- Create DirectX 11 basic ----
+
     HRESULT hr;
 
     //Describe our Buffer
@@ -17,7 +19,7 @@ bool OPTada_Render::Initialization(HWND hwnd_, int countOfBackBuffers_, int work
     // get display FPS
     DEVMODE dm;
     dm.dmSize = sizeof(DEVMODE);
-    EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &dm);
+    EnumDisplaySettings(nullptr, ENUM_CURRENT_SETTINGS, &dm);
 
     bufferDesc.Width                   = workspaceWidth_;
     bufferDesc.Height                  = workspaceHeight_;
@@ -63,9 +65,9 @@ bool OPTada_Render::Initialization(HWND hwnd_, int countOfBackBuffers_, int work
 
     //Create our Device and SwapChain
     if (S_OK != D3D11CreateDeviceAndSwapChain(
-        NULL,                     // default adapter
+        nullptr,                  // default adapter
         D3D_DRIVER_TYPE_HARDWARE, // use harware DirectX realization
-        NULL,                      
+        nullptr,
         createDeviceFlags,
         featureLevels,            // feature levels list
         _countof(featureLevels),  // count of futer levels
@@ -73,7 +75,7 @@ bool OPTada_Render::Initialization(HWND hwnd_, int countOfBackBuffers_, int work
         &swapChainDesc,
         &g_SwapChain,
         &g_Device_d3d11,
-        NULL,
+        nullptr,
         &g_DeviceContext_d3d11)) 
     {
         return false;
@@ -86,15 +88,106 @@ bool OPTada_Render::Initialization(HWND hwnd_, int countOfBackBuffers_, int work
     }
 
     //Create our Render Target
-    if (S_OK != g_Device_d3d11->CreateRenderTargetView(backBuffer, NULL, &g_RenderTargetView_d3d)) {
+    if (S_OK != g_Device_d3d11->CreateRenderTargetView(backBuffer, nullptr, &g_RenderTargetView_d3d)) {
         SafeRelease(backBuffer);
         return false;
     }
     SafeRelease(backBuffer);
 
     //Set our Render Target
-    g_DeviceContext_d3d11->OMSetRenderTargets(1, &g_RenderTargetView_d3d, NULL);
+    g_DeviceContext_d3d11->OMSetRenderTargets(1, &g_RenderTargetView_d3d, nullptr);
 
+    // ---- Depth | stencil buffer ----
+
+    // Create the depth buffer for use with the depth/stencil view.
+    D3D11_TEXTURE2D_DESC depthStencilBufferDesc;
+    ZeroMemory(&depthStencilBufferDesc, sizeof(D3D11_TEXTURE2D_DESC));
+    depthStencilBufferDesc.ArraySize          = 1;                             // count of textures
+    depthStencilBufferDesc.BindFlags          = D3D11_BIND_DEPTH_STENCIL;      // use like a Depth buffer
+    depthStencilBufferDesc.CPUAccessFlags     = 0;                             // No CPU access required.
+    depthStencilBufferDesc.Format             = DXGI_FORMAT_D24_UNORM_S8_UINT;
+    depthStencilBufferDesc.Width              = workspaceWidth_;
+    depthStencilBufferDesc.Height             = workspaceHeight_;
+    depthStencilBufferDesc.MipLevels          = 1;                             // Use 1 for a multisampled texture; or 0 to generate a full set of subtextures.
+    depthStencilBufferDesc.SampleDesc.Count   = 1;
+    depthStencilBufferDesc.SampleDesc.Quality = 0;
+    depthStencilBufferDesc.Usage              = D3D11_USAGE_DEFAULT;
+
+    // create texture for depth buffer
+    if (S_OK != g_Device_d3d11->CreateTexture2D(&depthStencilBufferDesc, nullptr, &g_DepthStencilBuffer_d3d)) {
+        return false;
+    }
+
+    // create stencil view, for depth buffer
+    if (S_OK != g_Device_d3d11->CreateDepthStencilView(g_DepthStencilBuffer_d3d, nullptr, &g_DepthStencilView_d3d)) {
+        return false;
+    }
+
+    // Setup depth/stencil state.
+    D3D11_DEPTH_STENCIL_DESC depthStencilStateDesc;
+    ZeroMemory(&depthStencilStateDesc, sizeof(D3D11_DEPTH_STENCIL_DESC));
+
+    depthStencilStateDesc.DepthEnable      = TRUE; // depth enable
+    depthStencilStateDesc.DepthWriteMask   = D3D11_DEPTH_WRITE_MASK_ALL;
+    depthStencilStateDesc.DepthFunc        = D3D11_COMPARISON_LESS;
+
+    depthStencilStateDesc.StencilEnable    = TRUE; // stencil enable
+    depthStencilStateDesc.StencilReadMask  = D3D11_DEFAULT_STENCIL_READ_MASK;
+    depthStencilStateDesc.StencilWriteMask = D3D11_DEFAULT_STENCIL_WRITE_MASK;
+
+    // Stencil operations if pixel is front-facing
+    depthStencilStateDesc.FrontFace.StencilFailOp      = D3D11_STENCIL_OP_KEEP;
+    depthStencilStateDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;
+    depthStencilStateDesc.FrontFace.StencilPassOp      = D3D11_STENCIL_OP_KEEP;
+    depthStencilStateDesc.FrontFace.StencilFunc        = D3D11_COMPARISON_ALWAYS;
+
+    // Stencil operations if pixel is back-facing
+    depthStencilStateDesc.BackFace.StencilFailOp      = D3D11_STENCIL_OP_KEEP;
+    depthStencilStateDesc.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_DECR;
+    depthStencilStateDesc.BackFace.StencilPassOp      = D3D11_STENCIL_OP_KEEP;
+    depthStencilStateDesc.BackFace.StencilFunc        = D3D11_COMPARISON_ALWAYS;
+
+    // create depth stencil state
+    if (S_OK != g_Device_d3d11->CreateDepthStencilState(&depthStencilStateDesc, &g_DepthStencilState_d3d)) {
+        return false;
+    }
+
+    // ---- Setup rasterizer ----
+
+    // Setup rasterizer state.
+    D3D11_RASTERIZER_DESC rasterizerDesc;
+    ZeroMemory(&rasterizerDesc, sizeof(D3D11_RASTERIZER_DESC));
+
+    rasterizerDesc.AntialiasedLineEnable = FALSE;
+    rasterizerDesc.CullMode              = D3D11_CULL_BACK;  // do not draw backfront triangles
+    rasterizerDesc.DepthBias             = 0;
+    rasterizerDesc.DepthBiasClamp        = 0.0f;
+    rasterizerDesc.DepthClipEnable       = TRUE;             // clip z coord
+    rasterizerDesc.FillMode              = D3D11_FILL_SOLID; // - draw triangles formed // D3D11_FILL_WIREFRAME - darw lines
+    rasterizerDesc.FrontCounterClockwise = FALSE;            // a triangle will be considered front-facing if its vertices are counter-clockwise on the render target
+    rasterizerDesc.MultisampleEnable     = FALSE;
+    rasterizerDesc.ScissorEnable         = FALSE;            // is scissor-rectangle culling. TRUE - enable
+    rasterizerDesc.SlopeScaledDepthBias  = 0.0f;
+
+    // Create the rasterizer state object.
+    hr = g_Device_d3d11->CreateRasterizerState(&rasterizerDesc, &g_RasterizerState_d3d);
+    if (FAILED(hr))
+    {
+        return -1;
+    }
+
+    // ---- Setup viewport ----
+
+    // Initialize the viewport to occupy the entire client area.
+    g_Viewport.Width = static_cast<float>(workspaceWidth_);
+    g_Viewport.Height = static_cast<float>(workspaceHeight_);
+    g_Viewport.TopLeftX = 0.0f;
+    g_Viewport.TopLeftY = 0.0f;
+    g_Viewport.MinDepth = 0.0f;
+    g_Viewport.MaxDepth = 1.0f;
+
+    // ---- ALL initialization done ----
+    
     return true;
 }
     
